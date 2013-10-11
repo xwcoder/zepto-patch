6fb70918989a322e960eba84e0f42576b0e54aa4
xwcoder <xwcoder@outlook.com>
2013-10-11 09:55:51 +0800

    patch.js
    * add Function.prototype.bind

--- src/patch.js
+++ src/patch.js
@@ -1,4 +1,3 @@
-//
 // @author xwcoder@outlook.com
 // zepto patch file
 // patch list:
@@ -139,3 +138,29 @@
         }
     } );
 } )( Zepto );
+
+( function () {
+     if (!Function.prototype.bind) {
+      Function.prototype.bind = function (oThis) {
+        if (typeof this !== "function") {
+          // closest thing possible to the ECMAScript 5 internal IsCallable function
+          throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+        }
+
+        var aArgs = Array.prototype.slice.call(arguments, 1), 
+            fToBind = this, 
+            fNOP = function () {},
+            fBound = function () {
+              return fToBind.apply(this instanceof fNOP && oThis
+                                     ? this
+                                     : oThis,
+                                   aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+
+        return fBound;
+      };
+    }   
+} )();
